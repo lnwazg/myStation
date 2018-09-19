70e3a17f1683ec5ee26109155fdd46dccb26f853
李楠 <lnwazg@126.com>
2018-09-19 10:56:04 +0800

    优化项目结构

--- src/main/java/com/lnwazg/controller/WorkInfoController.java
+++ src/main/java/com/lnwazg/controller/WorkInfoController.java
@@ -2,34 +2,46 @@ package com.lnwazg.controller;
 
 import java.util.Date;
 
-import org.apache.commons.lang3.Validate;
-
 import com.lnwazg.entity.WorkInfo;
 import com.lnwazg.kit.common.model.FrontObj;
 import com.lnwazg.kit.controllerpattern.Controller;
 import com.lnwazg.kit.singleton.B;
+import com.lnwazg.kit.validate.Validates;
 import com.lnwazg.service.WorkInfoService;
 
+/**
+ * Controller类
+ * @author nan.li
+ * @version 2018年9月19日
+ */
 @Controller("/workinfo")
 public class WorkInfoController extends ParentController
 {
     WorkInfoService workInfoService = B.g(WorkInfoService.class);
     
+    /**
+     * 测试HelloWorld
+     * @author nan.li
+     */
     void hello()
     {
         okHtml("helloWorld");
     }
     
+    /**
+     * 保存工作信息
+     * @author nan.li
+     */
     void saveWorkInfo()
     {
         FrontObj frontObj = new FrontObj();
         try
         {
             String content = getParam("content");
-            Validate.notEmpty(content);
+            Validates.validateNotEmpty(content, "content参数不能为空！");
             WorkInfo workInfo = new WorkInfo();
             workInfo.setContent(content).setCreateTime(new Date());
-            workInfoService.workInfoDao.insert(workInfo);
+            workInfoService.saveWorkInfo(workInfo);
             okJson(frontObj.success().setErrmsg("工作信息保存成功"));
         }
         catch (Exception e)
--- src/main/java/com/lnwazg/service/WorkInfoService.java
+++ src/main/java/com/lnwazg/service/WorkInfoService.java
@@ -1,9 +1,25 @@
 package com.lnwazg.service;
 
+import java.sql.SQLException;
+
 import com.lnwazg.dao.WorkInfoDao;
+import com.lnwazg.dbkit.anno.service.Transactional;
+import com.lnwazg.entity.WorkInfo;
 import com.lnwazg.kit.singleton.B;
 
+/**
+ * Service类
+ * @author nan.li
+ * @version 2018年9月19日
+ */
 public class WorkInfoService
 {
-   public WorkInfoDao workInfoDao = B.g(WorkInfoDao.class);
+    private WorkInfoDao workInfoDao = B.g(WorkInfoDao.class);
+    
+    @Transactional
+    public void saveWorkInfo(WorkInfo workInfo)
+        throws SQLException
+    {
+        workInfoDao.insert(workInfo);
+    }
 }
