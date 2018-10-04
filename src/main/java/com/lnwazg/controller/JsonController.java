8214001bf7c5341f9d42872ce905ff9e2cb2ad4c
lnwazg <lnwazg@126.com>
2018-10-04 21:36:19 +0800

    支持Json和Xml风格的注解

---
+++ src/main/java/com/lnwazg/controller/JsonController.java
@@ -0,0 +1,37 @@
+package com.lnwazg.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.lnwazg.httpkit.anno.JsonResponse;
+import com.lnwazg.kit.cache.JvmMemCacheLite;
+import com.lnwazg.kit.controllerpattern.Controller;
+import com.lnwazg.kit.singleton.B;
+import com.lnwazg.service.GuokeRssService;
+
+@Controller("/json")
+@JsonResponse
+public class JsonController extends ParentController
+{
+    GuokeRssService guokeRssService = B.g(GuokeRssService.class);
+    
+    String sayHello()
+    {
+        return "hello";
+    }
+    
+    @SuppressWarnings("unchecked")
+    List<String> rss()
+        throws SQLException
+    {
+        List<String> contentList = (List<String>)JvmMemCacheLite.get("rssListSqlite", 2, TimeUnit.MINUTES);
+        if (contentList == null)
+        {
+            guokeRssService.refreshRssContentCacheFromSqliteDB();
+            contentList = (List<String>)JvmMemCacheLite.get("rssListSqlite", 2, TimeUnit.MINUTES);
+        }
+        return contentList;
+    }
+    
+}
---
+++ src/main/java/com/lnwazg/controller/XmlController.java
@@ -0,0 +1,36 @@
+package com.lnwazg.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.lnwazg.httpkit.anno.XmlResponse;
+import com.lnwazg.kit.cache.JvmMemCacheLite;
+import com.lnwazg.kit.controllerpattern.Controller;
+import com.lnwazg.kit.singleton.B;
+import com.lnwazg.service.GuokeRssService;
+
+@Controller("/xml")
+@XmlResponse
+public class XmlController extends ParentController
+{
+    GuokeRssService guokeRssService = B.g(GuokeRssService.class);
+    
+    String sayHello()
+    {
+        return "hello";
+    }
+    
+    @SuppressWarnings("unchecked")
+    List<String> rss()
+        throws SQLException
+    {
+        List<String> contentList = (List<String>)JvmMemCacheLite.get("rssListSqlite", 2, TimeUnit.MINUTES);
+        if (contentList == null)
+        {
+            guokeRssService.refreshRssContentCacheFromSqliteDB();
+            contentList = (List<String>)JvmMemCacheLite.get("rssListSqlite", 2, TimeUnit.MINUTES);
+        }
+        return contentList;
+    }
+}
